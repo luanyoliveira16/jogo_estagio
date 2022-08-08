94a38c89cb147e82a1ea7efdfe361f155db1fb2c
Luany Oliveira <luanyoliveira16@gmail.com>
2022-08-08 18:02:56 -0300

    questoes, estilo e logica do jogo

---
+++ fedon/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+function App() {
+  return (
+   <>
+    <h1>Produzindo</h1>
+   </>
+  );
+}
+
+export default App;
---
+++ questions.js
@@ -0,0 +1,104 @@
+let questions = [
+          {
+              question: 'O filósofo deve temer a morte?',
+              options: [
+                  'Sim. É dever do filósofo preservar a vida e aos tolos cabe a alegria diante da perspectiva da morte.',
+                  'Não. Porque os homens que se dedicam à filosofia estão justamente se preparando para a morte.',
+                  'Sim. O caráter lógico da filosofia depende da interação entre o corpo e a alma, portanto é necessário estar vivo pra filosofar. ',
+                  'Não. Porque o que deve importar verdadeiramente para o filósofo é o legado de seus pensamentos.'
+              ],
+              answer: 1
+          },
+          {
+              question: 'O corpo é um obstáculo para o pensamento?',
+              options: [
+                  'Não. O corpo é tão necessário quanto a alma.',
+                  'Sim. O corpo prejudica o conhecimento da alma, mas não impede que seja alcançado durante a vida.',
+                  'Sim. O corpo é o obstáculo do conhecimento da alma, portanto o conhecimento só poderá ser alcançado após a morte.'
+              ],
+              answer: 2
+          },
+          {
+              question: 'Sobre o destino das almas...',
+              options: [
+                  'No momento de sua libertação arrasta consigo algo corpóreo ',
+                  'Após a morte, a alma dirige-se para o que lhe parece invisível, divino e imortal.',
+                  'Não faz diferença o modo como vivemos, pois independente da maneira em que lidamos com o corpo, a alma sempre será pura.'
+              ],
+              answer: 1
+          },
+          {
+              question: 'A alma é imortal.',
+              options: [
+                  'Verdade',
+                  'Falso'
+              ],
+              answer: 0
+          },
+          {
+              question: 'O que é a morte?',
+              options: [
+                  'É o fim de tudo.',
+                  'É a descoberta do mundo das ideias.',
+                  'É a separação entre a alma e o corpo.'
+              ],
+              answer: 2
+          },
+          {
+              question: 'Sobre a reminiscência...',
+              options: [
+                  'Diz respeito à purificação da alma.',
+                  '',
+                  'function minhaFuncao()'
+              ],
+              answer: 2
+          },
+          {
+              question: 'Sobre o filósofo, qual é a incorreta?',
+              options: [
+                  'O filósofo não se preocupa com a riqueza: a pobreza não lhes infunde medo.',
+                  'O filósofo não se preocupa com a glória ou honra: o infortúnio não o atemoriza.',
+                  'O filósofo não se preocupa em purificar sua alma.', 
+                  'O filósofo afasta-se dos prazeres, dos desejos, dos incômodos e dos terrores.'
+              ],
+              answer: 2
+          },
+          {
+              question: 'Somos livres?',
+              options: [
+                  'Não',
+                  'if i == 5 then',
+                  'if (i == 5)',
+                  'if i = 5 then'
+              ],
+              answer: 2
+          },
+          {
+              question: 'Respondendo a teoria a Alma-Harmonia...',
+              options: [
+                  'Está correta, pois a alma é uma harmonia entre as partes do corpo.',
+                  'Está correta, pois se o corpo é destruído a harmonia não pode sobreviver.',
+                  'Está incorreta, pois a harmonia não pode existir antes dos elementos dos quais vem a ser composta.',
+                  'Está incorreta, pois está de acordo com reminiscência'
+              ],
+              answer: 2
+          },
+          {
+              question: 'Como o loop while começa?',
+              options: [
+                  'while (i <= 10)',
+                  'while (i <= 10; i++)',
+                  'while i = 1 to 10'
+              ],
+              answer: 0
+          },
+          {
+              question: 'Gostou do projeto?',
+              options: [
+                  'Sim',
+                  'Não',
+                  'Talvez'
+              ],
+              answer: 0
+          },
+      ];
